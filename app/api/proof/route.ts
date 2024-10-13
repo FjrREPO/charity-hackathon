802110843a8d5589a1ff75f8605255d479a6b615
azizrosyid <azizrosyid783@gmail.com>
2024-10-13 14:48:53 +0700

    Refactor TableProof component and add invoice input field

--- app/api/proof/route.ts
+++ app/api/proof/route.ts
@@ -1,68 +1,90 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 import { ReclaimClient } from "@reclaimprotocol/zk-fetch";
-import {
-    transformForOnchain,
-    verifyProof,
-} from "@reclaimprotocol/js-sdk";
+import { transformForOnchain, verifyProof } from "@reclaimprotocol/js-sdk";
 
 export async function POST(req: NextRequest) {
-    try {
-        const body = await req.json();
-        // invoice from id item
-        const { invoice } = body;
+  try {
+    const body = await req.json();
+    // invoice from id item
+    const { invoice } = body;
 
-        if (!invoice) {
-            return NextResponse.json({ error: "Missing invoice" }, { status: 400 });
-        }
-
-        const reclaimId = process.env.RECLAIM_ID;
-        const reclaimSecret = process.env.RECLAIM_SECRET;
-        const verifyUrl = process.env.API_PROOF_URL;
-
-        if (!reclaimId || !reclaimSecret) {
-            return NextResponse.json({ error: "Missing reclaim credentials" }, { status: 400 });
-        }
-
-        if (!verifyUrl) {
-            return NextResponse.json({ error: 'Missing verify URL' }, { status: 400 });
-        }
-
-        const client = new ReclaimClient(reclaimId, reclaimSecret);
-
-        const publicOptions = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json, text/plain, */*',
-                'Content-Type': 'application/json',
-            }
-        };
-
-        const proof = await client.zkFetch(
-            "https://run.mocky.io/v3/81c98130-f8a8-4b38-9ab8-284df41f7829",
-            publicOptions,
-            {
-                responseMatches: [
-                    {
-                        "type": "regex",
-                        "value": "\"id\":\\s*(?<id>[\\d.]+)"
-                    }
-                ],
-            }
-        );
-
-        if (!proof) {
-            return NextResponse.json({ error: 'Failed to generate proof' }, { status: 500 });
-        }
-
-        const isProofVerified = await verifyProof(proof);
-        const proofData = transformForOnchain(proof);
-
-        return NextResponse.json({
-            proofData,
-            isProofVerified,
-        });
-    } catch (error) {
-        console.error('Error in /api/proof:', error);
-        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    if (!invoice) {
+      return NextResponse.json({ error: "Missing invoice" }, { status: 400 });
     }
+
+    const reclaimId = process.env.RECLAIM_ID;
+    const reclaimSecret = process.env.RECLAIM_SECRET;
+    const verifyUrl = process.env.API_PROOF_URL;
+
+    if (!reclaimId || !reclaimSecret) {
+      return NextResponse.json(
+        { error: "Missing reclaim credentials" },
+        { status: 400 }
+      );
+    }
+
+    if (!verifyUrl) {
+      return NextResponse.json(
+        { error: "Missing verify URL" },
+        { status: 400 }
+      );
+    }
+
+    const client = new ReclaimClient(reclaimId, reclaimSecret);
+
+    const publicOptions = {
+      method: "GET",
+      headers: {
+        accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+    };
+
+    // {
+    //     "id": 1,
+    //     "name": "Paket setelan 6 pcs anak laki-laki perempuan unisex usia 0 - 7 Tahun - CEWEK SEMUA, S",
+    //     "link": "https://www.tokopedia.com/foyakids/paket-setelan-6-pcs-anak-laki-laki-perempuan-unisex-usia-0-7-tahun-cewek-semua-s-b81cf",
+    //     "image": "https://images.tokopedia.net/img/cache/700/VqbcmM/2023/12/14/e5403d71-aed3-4560-9ac6-b7be1623ac74.jpg",
+    //     "price": 0.0001,
+    //     "source": "Tokopedia"
+    //   }
+
+    const link = `https://defiant-morna-rosyid-7ea20110.koyeb.app/api/donation/1`;
+    const proof = await client
+      .zkFetch(link, publicOptions, {
+        responseMatches: [
+          {
+            type: "regex",
+            value: '.*"id":\\s(?<id>\\d+),.*',
+          },
+        ],
+      })
+      .catch((error) => {
+        console.error("Error in /api/proof:", error);
+        return null;
+      });
+
+    if (!proof) {
+      return NextResponse.json(
+        { error: "Failed to generate proof" },
+        { status: 500 }
+      );
+    }
+
+    const isProofVerified = await verifyProof(proof);
+    const proofData = transformForOnchain(proof);
+
+    console.log("Proof data:", proof);
+
+    return NextResponse.json({
+      proofData,
+      isProofVerified,
+    });
+  } catch (error) {
+    console.error("Error in /api/proof:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
